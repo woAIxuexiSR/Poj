d98f63736ce628f300aa68c4b5bb04446431c897
woAIxuexiSR <1600017705@pku.edu.cn>
2018-08-21 15:04:16 +0800

    123

--- Muddy Fields.cpp
+++ Muddy Fields.cpp
@@ -2,6 +2,43 @@
 using namespace std;
 int r,c;
 int matrix[50][50];
+int rcnt,ccnt;
+int rmat[50][50],cmat[50][50];
+bool g[1025][1025];
+bool visited[1025];
+int cx[1025],cy[1025];
+
+bool dfs(int u)
+{
+    for(int i = 0;i < ccnt; ++i)
+    {
+        if(g[u][i] && !visited[i])
+        {
+            visited[i] = 1;
+            if(cy[i] == -1 || dfs(cy[i]))
+            {
+                cx[u] = i; cy[i] = u;
+                return true;
+            }
+        }
+    }
+    return 0;
+}
+
+int maxmatch()
+{
+    int ans = 0;
+    memset(cx,-1,sizeof(cx));
+    memset(cy,-1,sizeof(cy));
+    for(int i = 0; i < rcnt; ++i)
+        if(cx[i] == -1)
+        {
+            memset(visited,0,sizeof(visited));
+            if(dfs(i)) ++ans;
+        }
+    return ans;
+}
+
 int main()
 {
     cin >> r >> c;
@@ -16,6 +53,41 @@ int main()
            else matrix[i][j] = 0; 
         }
     }
-    
+
+    rcnt = ccnt = 0;
+    for(int i = 0;i < r;++i)
+    {
+        bool flag = 0;
+        for(int j = 0;j < c;++j)
+        {
+            if(matrix[i][j] == 0 && !flag)
+                flag = 1, rmat[i][j] = rcnt++;
+            else if(matrix[i][j] == 0 && flag)
+                rmat[i][j] = rcnt;
+            else
+                flag = 0;
+        }
+    }
+
+    for(int j = 0;j < c;++j)
+    {
+        bool flag = 0;
+        for(int i = 0;i < r;++i)
+        {
+            if(matrix[i][j] == 0 && !flag)
+                flag = 1, cmat[i][j] = ccnt++;
+            else if(matrix[i][j] == 0 && flag)
+                cmat[i][j] = ccnt;
+            else
+                flag = 0;
+        }
+    }
+
+    for(int i = 0; i < r; ++i)
+        for(int j = 0; j < c; ++j)
+            if(matrix[i][j]==0)
+                g[rmat[i][j]][cmat[i][j]] = 1;
+
+    cout << maxmatch() << endl;
     return 0;
 }
